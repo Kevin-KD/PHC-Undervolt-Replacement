3ab27c97eb189d6720ca323d27529695d6009a75
Kevin-KD <17510960+Kevin-KD@users.noreply.github.com>
2020-12-08 21:38:52 -0500

    Add comments

--- script.sh
+++ script.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+# PHC-Undervolt-Replacement by Kevin-KD
+# Simple script to undervolt CPU
+# Run with root privilege and nice
+# Check Github page for more details
+# https://github.com/Kevin-KD/PHC-Undervolt-Replacement
 
+# Load msr driver into kernel
 modprobe msr
 
+# Set ondemand governor to ignore nice load
 echo -n 1 > /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load
 
+# Get the number of CPU threads
 cpulist = $(grep processor /proc/cpuinfo | awk '{print $3}')
 
+# Infinite loop to modify voltage on the fly
 while true
 do
+	# Modify voltage on every thread
 	for i in $cpulist
 	do
-		currentFID = $(rdmsr -0 0x198 | cut -b 13-14) 
+		# Find current frequency set by ondeamnd governor
+		currentFID = $(rdmsr -0 0x198 | cut -b 13-14)
 		case $currentFID in
+			# If FID is 0e then set VID to 29
 			0e)
 				wrmsr -p$i 0x199 0x0e29
 				;;
