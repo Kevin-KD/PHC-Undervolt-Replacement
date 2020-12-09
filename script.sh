e04eac1a0e4c68f8ac18aa884e400177dba7125c
Kevin-KD <17510960+Kevin-KD@users.noreply.github.com>
2020-12-09 13:58:36 -0500

    Fix _load_avg_1 and calculate threshold based on number of threads

--- script.sh
+++ script.sh
@@ -10,6 +10,7 @@ modprobe msr
 
 # Get the number of CPU threads
 _cpu_list=$(grep processor /proc/cpuinfo | awk '{print $3}')
+_num_threads=${#_cpu_list[@]}
 
 # Set to userspace governor
 for i in $_cpu_list
@@ -17,33 +18,40 @@ do
 	echo userspace >/sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
 done
 
+# Calculate load threshold for frequency change
+_threshold_1=$(bc <<< 'scale=2; 0.2/$_num_threads')
+_threshold_2=$(bc <<< 'scale=2; 0.4/$_num_threads')
+_threshold_3=$(bc <<< 'scale=2; 0.6/$_num_threads')
+_threshold_4=$(bc <<< 'scale=2; 0.8/$_num_threads')
+_threshold_5=$(bc <<< 'scale=2; 1.0/$_num_threads')
+
 # Infinite loop to modify frequency and voltage
 while true
 do
 	# Find CPU load average
 	_load_avg=$(cat /proc/loadavg)
 	_load_avg_array=($_load_avg)
-	_load_avg_1=${_load_avg[0]}
+	_load_avg_1=${_load_avg_array[0]}
 	
 	# Modify frequency and voltage based on CPU load
-	if [ "`echo "${_load_avg_1} < 0.2" | bc`" -eq 1 ]
+	if [ "`echo "${_load_avg_1} < $_threshold_1" | bc`" -eq 1 ]
 	then
 		# Set frequency to 0.8 GHz on all cores
 		echo 800000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 		wrmsr -a 0x199 0x8811
-	elif [ "`echo "${_load_avg_1} < 0.4" | bc`" -eq 1 ]
+	elif [ "`echo "${_load_avg_1} < $_threshold_2" | bc`" -eq 1 ]
 	then
 		echo 1200000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 		wrmsr -a 0x199 0x0617
-	elif [ "`echo "${_load_avg_1} < 0.6" | bc`" -eq 1 ]
+	elif [ "`echo "${_load_avg_1} < $_threshold_3" | bc`" -eq 1 ]
 	then
 		echo 1600000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 		wrmsr -a 0x199 0x081b
-	elif [ "`echo "${_load_avg_1} < 0.8" | bc`" -eq 1 ]
+	elif [ "`echo "${_load_avg_1} < $_threshold_4" | bc`" -eq 1 ]
 	then
 		echo 2000000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 		wrmsr -a 0x199 0x0a1e
-	elif [ "`echo "${_load_avg_1} < 1.0" | bc`" -eq 1 ]
+	elif [ "`echo "${_load_avg_1} < $_threshold_5" | bc`" -eq 1 ]
 	then
 		echo 2600000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 		wrmsr -a 0x199 0x0d22
