cbbf7e25d628edd45940e7792475894e083e0c1a
Kevin-KD <17510960+Kevin-KD@users.noreply.github.com>
2020-12-08 21:22:06 -0500

    Implement Dynamic Frequency Scaling

--- script.sh
+++ script.sh
@@ -1,4 +1,35 @@
-sudo sh -c "echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
-sudo sh -c "echo performance >/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor"
-sudo modprobe msr
-sudo wrmsr -a 0x199 0x4F1E
+#!/bin/bash
+
+modprobe msr
+
+echo -n 1 > /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load
+
+cpulist = $(grep processor /proc/cpuinfo | awk '{print $3}')
+
+while true
+do
+	for i in $cpulist
+	do
+		currentFID = $(rdmsr -0 0x198 | cut -b 13-14) 
+		case $currentFID in
+			0e)
+				wrmsr -p$i 0x199 0x0e29
+				;;
+			0d)
+				wrmsr -p$i 0x199 0x0d22
+				;;
+			0a)
+				wrmsr -p$i 0x199 0x0a1e
+				;;
+			08)
+				wrmsr -p$i 0x199 0x081b
+				;;
+			06)
+				wrmsr -p$i 0x199 0x0617
+				;;
+			88)
+				wrmsr -p$i 0x199 0x8811
+				;;
+		esac
+	done
+done
